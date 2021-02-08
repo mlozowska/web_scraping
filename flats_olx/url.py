1869814656ef33043b719074bdc92f14ac1a12ba
Magdalena Lozowska <60215258+mlozowska@users.noreply.github.com>
2021-02-08 20:05:47 +0100

    Create url.py

---
+++ flats_olx/url.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/warszawa/"
+
+response = requests.get(url)
+
+data = response.text
+
+soup = BeautifulSoup(data,'html.parser')
+
+jobs = soup.find_all('div',{'class':'offer-wrapper'})
+
+
+for job in jobs:
+    title = job.find('strong').text
+  
+    print(title)
