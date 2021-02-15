a892b57cef3932289c162f5377edb9ec6ca3e308
Magdalena Lozowska <60215258+mlozowska@users.noreply.github.com>
2021-02-15 21:39:34 +0100

    Add files via upload

---
+++ otodom/flat_details.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/warszawa/"
+
+response = requests.get(url)
+
+data = response.text
+
+soup = BeautifulSoup(data, 'html.parser')
+
+jobs = soup.find_all('div', {'class': 'content'})
+
+for job in jobs:
+    title = job.find('strong')
+    # location = job.find('span', {'data_icon':'location-filled'}).text
+    link = job.find('span').get('href')
+    date = job.find('span', {'data-icon':'clock'}).text
+
+    print(title, link)
+
